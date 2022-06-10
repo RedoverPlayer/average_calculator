c7b8b3b77d5e69d33f19bf1a179d4e2fbe416cec
RedoverPlayer <67553709+RedoverPlayer@users.noreply.github.com>
2022-06-10 19:42:35 +0200

    add request info get and improve ui

--- content.js
+++ content.js
@@ -1,194 +1,250 @@
-// Calculates the averages and some other stuff
-function calculateAverage() {
-    // gets the shadowRoot of the body (yes marks are in there idk why but they are)
-    if (document.querySelector("releve-but") == null) {
-        return;
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
     }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
+
+function addSection(shadowRoot, title) {
+    var section = shadowRoot.appendChild(document.createElement('section'));
+    section.className = "section";
+
+    var sectionTitle = section.appendChild(document.createElement('h2'));
+    sectionTitle.innerText = title;
+
+    return section;
+}
+
+function addRessource(section) {
+    var ressourceDiv = section.appendChild(document.createElement('div'));
+    ressourceDiv.style.marginTop = "0.2em";
+    ressourceDiv.className = "ressource";
+
+    return ressourceDiv;
+}
+
+function addRessourceTitle(ressourceDiv, title) {
+    ressourceTitleContainer = ressourceDiv.appendChild(document.createElement('div'));
+    ressourceTitleContainer.style.display = "flex";
+    ressourceTitleContainer.style.justifyContent = "space-between";
+    ressourceTitleContainer.style.backgroundColor = "#00be82";
+    ressourceTitleContainer.style.padding = "0.2em";
+    ressourceTitleContainer.style.borderRadius = "0.2em";
+    ressourceTitleContainer.style.color = "white";
+    ressourceTitleContainer.style.paddingLeft = "2em";
+    ressourceTitleContainer.style.paddingRight = "2em";
+
+    ressourceTitle = ressourceTitleContainer.appendChild(document.createElement('div'));
+    ressourceTitle.style.fontSize = "1.1em";
+    ressourceTitle.innerText = title;
+
+    return ressourceTitleContainer;
+}
+
+function addEvalContainer(section) {
+    var evalContainer = section.appendChild(document.createElement('div'));
+    evalContainer.style.display = "flex";
+    evalContainer.style.flexDirection = "column";
+    evalContainer.style.marginLeft = "2em";
+
+    return evalContainer;
+}
+
+function addEvaluation(section, evaluation, poids, isUE=false) {
+    var eval = section.appendChild(document.createElement('div'));
+    eval.innerText = evaluation.description;
+    eval.title = "Min: " + evaluation.note.min + ", Max: " + evaluation.note.max;
+    eval.style.borderBottom = "1px solid black";
+    eval.style.display = "flex";
+    eval.style.justifyContent = "space-between";
+    eval.style.gap = "1em"; 
+    eval.style.paddingLeft = "0.2em";
+    eval.style.paddingRight = "0.2em";
+
+    var evalNote = eval.appendChild(document.createElement('div'));
+    evalNote.innerText = evaluation.note.value;
+    evalNote.style.marginLeft = "auto";
+    evalNote.style.width = "4em";
+
+    var evalMoy = eval.appendChild(document.createElement('div'));
+    evalMoy.innerText = "Moy. " + evaluation.note.moy;
+    evalMoy.style.color = "rgb(120, 120, 120)";
+    evalMoy.style.width = "5em";
+
+    var evalCoef = eval.appendChild(document.createElement('div'));
+    evalCoef.innerText = "Coeff. " + evaluation.coef;
+    evalCoef.style.color = "rgb(120, 120, 120)";
+    evalCoef.style.width = "5.6em";
+
+    if (isUE) {
+        var evalPoids = eval.appendChild(document.createElement('div'));
+        evalPoids.innerText = "Poids. " + poids;
+        evalPoids.style.color = "rgb(120, 120, 120)";
+        evalPoids.style.width = "5.6em";
+    }
+
+    if (parseFloat(evaluation.note.value) >= parseFloat(evaluation.note.max)) {
+        evalNote.style.color = "green";
+        addTag(evalNote, "MAX");
+    } else if (parseFloat(evaluation.note.value) <= parseFloat(evaluation.note.min)) {
+        evalNote.style.color = "red";
+        addTag(evalNote, "MIN");
+    }
+}
+
+function addTag(elem, content) {
+    var tag = document.createElement("div");
+    tag.innerText = content;
+    tag.style.display = "inline";
+    tag.style.fontSize = "11px";
+    elem.appendChild(tag);
+}
+
+function addToUE(ues, ressource, evaluation) {
+    for (poid of Object.entries(evaluation.poids)) {
+        if (!(poid[0] in ues)) {
+            ues[poid[0]] = [];
+        }
+        if (poid[1] > 0) {
+            if (!(ressource[0] in ues[poid[0]])) {
+                ues[poid[0]][ressource[0]] = [];
+            }
+            ues[poid[0]][ressource[0]].push({"ressource": ressource[1], "evaluation": evaluation});
+        }
+    }
+}
+
+function handleInfos(data) {
     var root = document.querySelector("releve-but").shadowRoot;
+    root.querySelector(".releve>section:nth-child(3)").style.display = "block"; // display global avg
 
-    // Do not recalculate page if already done
-    if (root.querySelectorAll('section[id="ue_data"]').length == 0) {
-        var UE = {};
-    
-        // Iterates over all modules
-        for (var module of root.querySelectorAll('div[id*="Module_"]')) {
-            var marks = 0;
-            var coeffs = 0;
-    
-            // Iterates over all evaluations of the module
-            for (var eval of module.getElementsByClassName("eval")) {
-                // Text inside eval div
-                var data = eval.querySelectorAll("div")[1].innerText.split("\n");
-    
-                var mark = parseFloat(data[0].trim());
-                var coeff = parseFloat(data[1].replace("Coef.", "").trim());
-    
-                // Verifies if a mark is valid
-                if (!isNaN(mark) && !isNaN(coeff)) {
-                    marks += mark * coeff;
-                    coeffs += coeff;
-                }
+    // delete existing content
+    var sections = root.querySelectorAll("section");
+    for (section of sections) {
+        try {
+            if (section.querySelector("div").querySelector("h2").innerText == "Ressources" || section.querySelector("div").querySelector("h2").innerText == "SAÉ") {
+                section.remove();
             }
-    
-            // Displays the average of the current module
-            if (!module.querySelector('div[class="module"]').innerHTML.includes("average_calculator")) {
-                module.querySelector('div[class="module"]').innerHTML += '<div class="average_calculator" style="display: block !important">' + (marks / coeffs).toFixed(2) + "</div>";
-            }
-
-            // Adds the current module to the corresponding UE
-            addToUE(UE, module, (marks / coeffs).toFixed(2));
-        }
-    
-        // Displays the general average (it's already there just not showed)
-        root.querySelector(".releve>section:nth-child(3)").style.display = "block";
-    
-        // Generates the entire UE section
-        if (root.querySelectorAll('section[id="ue_data"]').length == 0) {
-            genUESection(UE, root);
+        } catch {
+            continue;
         }
     }
-}
 
-// Adds a module to an UE
-function addToUE(UE, module, average) {
-    var UEData = getUE(module);
-    
-    if (UEData != null) {
-        var UEName = UEData[0];
-        var weightInUE = UEData[1];
+    var data = JSON.parse(data);
+    data = data["relev\u00e9"];
+    var ues = {}
+
+    // Add ressources
+    var rSection = addSection(root, "Ressources");
+
+    for (var ressource of Object.entries(data.ressources)) {
+        var ressourceDiv = addRessource(rSection);
+        var ressourceTitleContainer = addRessourceTitle(ressourceDiv, ressource[0] + " - " + ressource[1].titre);
+        var evalContainer = addEvalContainer(ressourceDiv);
         
-        // If the UE doesn't exist yet, create it
-        if (UE[UEName] == undefined) {
-            UE[UEName] = [];
-        }
-    
-        UE[UEName].push({"average": average, "weight": weightInUE, "elem": module});
-    }
-}
-
-// Gets the the UE the module belongs to
-function getUE(module) {
-    var data = module.querySelector('div[class="complement"]');
-
-    // Some modules don't have any evals
-    if (data != null) {
-        // Iterates through the eval info to find the UE the module belongs to
-        var infos = data.querySelectorAll("div");
-
-        for (let index in infos) {
-            if (infos[parseInt(index) + 1] != null && infos[index].innerHTML.includes("Poids") && parseFloat(infos[parseInt(index) + 1].innerText.trim()) != 0.0) {
-                var UEName = infos[index].innerText.replace("Poids ", "").trim();
-                var weightInUE = parseFloat(infos[parseInt(index) + 1].innerText.trim());
-                break;
+        var sum = 0;
+        var coefSum = 0;
+        
+        for (var evaluation of ressource[1].evaluations) {
+            if (!isNaN(parseFloat(evaluation.coef)) && !isNaN(parseFloat(evaluation.note.value))) {
+                coefSum += parseFloat(evaluation.coef);
+                sum += parseFloat(evaluation.coef) * parseFloat(evaluation.note.value);
             }
+            
+            addToUE(ues, ressource, evaluation);
+            addEvaluation(evalContainer, evaluation);
         }
 
-        return [UEName, weightInUE];
-    } else {
-        return null;
-    }
-}
-
-// Generates the UE section
-function genUESection(UE, root) {
-    var main = root.querySelector('main[class="releve"]')
-    
-    var currentSection = main.appendChild(document.createElement('section'));
-    currentSection.id = "ue_data";
-
-    addTitle(currentSection, "UE");
-
-    var UEContainer = currentSection.appendChild(document.createElement('div'));
-    UEContainer.className = "evaluations";
-
-    for (var [UEName, UEModules] of Object.entries(UE)) {
-        addUE(UE, UEContainer, UEName, UEModules);
-    }
-
-}
-
-// Add an UE to the UE section
-function addUE(UE, UEContainer, UEName, UEModules) {
-    var UE = UEContainer.appendChild(document.createElement('div'));
-    UE.id = UEName;
-    UE.addEventListener('click', function(event) { UEClick(event); });
- 
-    // Adds the title (name of the UE) to the UE
-    var UETitle = UE.appendChild(document.createElement('div'));
-    UETitle.className = "module";
-    UETitle.innerHTML = `
-        <h3 style="background: none">` + UEName + `</h3>
-        <div></div>
-        <div></div>`;
-
-    // Variables for the UE average
-    var modulesMarks = 0.0;
-    var modulesWeight = 0.0;
-
-    // Iterates over the modules of the UE
-    for (var module of UEModules) {
-        // Adds the module to the UE
-        var UEModule = UE.appendChild(document.createElement('div'));
-        UEModule.id = "UEModule_" + UEName;
-        UEModule.style.marginLeft = "40px";
-        UEModule.innerHTML += module["elem"].innerHTML;
-
-        // Increment UE average if the module has an average
-        if (!isNaN(module["average"]) && !isNaN(module["weight"])) {
-            modulesMarks += parseFloat(module["average"]) * parseFloat(module["weight"]);
-            modulesWeight += parseFloat(module["weight"]);
+        var avg = sum / coefSum;
+        ressourceTitleContainer.appendChild(document.createElement('div')).innerText = avg.toFixed(2);
+        if (avg < 10) {
+            ressourceTitleContainer.style.backgroundColor = "#ff4600";
         }
     }
 
-    // Adds the average of the UE to the UE
-    var UEAvg = (modulesMarks / modulesWeight).toFixed(2)
-    UETitle.innerHTML += '<div class="average_calculator" style="display: block !important">' + UEAvg + '</div>';
+    // add SAEs
+    var saeSection = addSection(root, "SA\u00c9");
 
-    // Changes the color of the UE depending of the average
-    if (UEAvg < 8) {
-        UETitle.style.backgroundColor = "red";
-    } else if (UEAvg < 10) {
-        UETitle.style.backgroundColor = "orange";
-    } else {
-        UETitle.style.backgroundColor = "green";
+    for (var sae of Object.entries(data.saes)) {
+        var saeDiv = addRessource(saeSection);
+        var saeTitleContainer = addRessourceTitle(saeDiv, sae[0] + " - " + sae[1].titre);
+        saeTitleContainer.style.backgroundColor = "#ffc828";
+        saeTitleContainer.style.color = "black";
+        var saeEvalContainer = addEvalContainer(saeDiv);
+
+        var sum = 0;
+        var coefSum = 0;
+
+        for (var evaluation of sae[1].evaluations) {
+            if (!isNaN(parseFloat(evaluation.coef)) && !isNaN(parseFloat(evaluation.note.value))) {
+                coefSum += parseFloat(evaluation.coef);
+                sum += parseFloat(evaluation.coef) * parseFloat(evaluation.note.value);
+            }
+
+            addToUE(ues, sae, evaluation);
+            addEvaluation(saeEvalContainer, evaluation);
+        }
+        
+        var avg = sum / coefSum;
+        saeTitleContainer.appendChild(document.createElement('div')).innerText = avg.toFixed(2);
+        if (avg < 10) {
+            saeTitleContainer.style.backgroundColor = "#ff4600";
+        }
+    }
+
+    // add UEs
+    var ueSection = addSection(root, "UE - Ces calculs sont à titre indicatifs et peuvent être différents de ceux de l'établissement. Merci de vérifier ces calculs par vous-même. (IUT Average Mark Calculator).");
+
+    for (var ue of Object.entries(ues)) {
+        var ueDiv = addRessource(ueSection);
+        var ueTitleContainer = addRessourceTitle(ueDiv, ue[0]);
+        ueTitleContainer.style.backgroundColor = "rgb(1, 114, 77)";
+        var ueEvalContainer = addEvalContainer(ueDiv);
+
+        var UEsum = 0;
+        var UEcoefSum = 0;
+
+        for (var ressource of Object.entries(ue[1])) {
+            var ueRessDiv = addRessource(ueEvalContainer);
+            var ueRessTitleContainer = addRessourceTitle(ueRessDiv, ressource[0] + " - " + ressource[1][0].ressource.titre);
+            var ueRessEvalContainer = addEvalContainer(ueRessDiv);
+
+            var sum = 0;
+            var coefSum = 0;
+
+            for (var item of ressource[1]) {
+                if (!isNaN(parseFloat(item.evaluation.coef)) && !isNaN(parseFloat(item.evaluation.note.value))) {
+                    UEcoefSum += parseFloat(item.evaluation.poids[ue[0]]);
+                    UEsum += parseFloat(item.evaluation.poids[ue[0]]) * parseFloat(item.evaluation.note.value);
+                }
+                if (!isNaN(parseFloat(item.evaluation.coef)) && !isNaN(parseFloat(item.evaluation.note.value))) {
+                    coefSum += parseFloat(item.evaluation.coef);
+                    sum += parseFloat(item.evaluation.coef) * parseFloat(item.evaluation.note.value);
+                }
+
+                addEvaluation(ueRessEvalContainer, item.evaluation, item.evaluation.poids[ue[0]], true);
+            }
+
+            var avg = sum / coefSum;
+            ueRessTitleContainer.appendChild(document.createElement('div')).innerText = avg.toFixed(2);
+            // if (avg < 10) {
+            //     ueRessTitleContainer.style.backgroundColor = "#ff4600";
+            // }
+        }
+
+        var UEavg = UEsum / UEcoefSum;
+        ueTitleContainer.appendChild(document.createElement('div')).innerText = UEavg.toFixed(2);
+        if (UEavg < 8) {
+            ueTitleContainer.style.backgroundColor = "orange";
+        }
+        if (UEavg < 10) {
+            ueTitleContainer.style.backgroundColor = "red";
+        }
     }
 }
 
-// List expand
-function listClick(event) {
-    // Gets the clicked element
-    if (event.target.parentElement.parentElement.tagName == "SECTION") {
-        var target = event.target.parentElement.parentElement;
-    } else if (event.target.parentElement.parentElement.parentElement.tagName == "SECTION") {
-        var target = event.target.parentElement.parentElement.parentElement;
-    }
-
-    if (target.tagName == "SECTION") {
-        target.classList.toggle("listeOff");
-    }
-}
-
-// Expand feature
-function UEClick(event) {
-    if (event.target.tagName == "DIV" && event.target.parentElement.id.includes("UE")) {
-        event.target.parentElement.classList.toggle("listeOff");
-    }
-}
-
-// Adds a title to a section
-function addTitle(section, title) {
-    var tmpDiv = section.appendChild(document.createElement('div'));
-    tmpDiv.innerHTML += '<h2>' + title + '</h2>';
-    
-    var listButton = tmpDiv.appendChild(document.createElement('div'));
-    listButton.className = "CTA_Liste";
-    listButton.innerHTML = `
-    Liste <svg xmlns="http://www.w3.org/2000/svg"  width="26" height="26" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-            <path d="M18 15l-6-6-6 6"></path>
-        </svg>`;
-    listButton.addEventListener('click', function(event) { listClick(event); });
-}
-
-// Js takes time to load (not mine, mine is beautiful okay), so there is multiple tries to calculate average
-// In addition to this, if you change semester, the page is not reloaded (thanks all-js menus) so I have to poll the page
-setInterval(calculateAverage, 1000);
+httpGetAsync("https://notes.iut.u-bordeaux.fr/services/data.php?q=dataPremi%C3%A8reConnexion", handleInfos);
