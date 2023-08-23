68f21695f228a40a8f59fc1c0372c2a9a4d46a58
Kensaa <kensadevs@gmail.com>
2023-08-23 20:20:23 +0200

    refactoring, cleaning and formating in settings folder

--- settings/update-ues.js
+++ settings/update-ues.js
@@ -1,35 +1,41 @@
 function updateUEs(data, currentSemester) {
     // Load current semester data
-    const semesterID = `semesterUEs${currentSemester}`;
-    chrome.storage.sync.get(semesterID).then((result) => {
-        let ues = result[semesterID];
-        if (ues === undefined) { ues = {}; }
+    const semesterID = `semesterUEs${currentSemester}`
+    chrome.storage.sync.get(semesterID).then(result => {
+        const ues = result[semesterID] ?? {}
 
-        addRessources(data["relevé"]["ressources"], ues);
-        addRessources(data["relevé"]["saes"], ues);
+        addRessources(data['relevé']['ressources'], ues)
+        addRessources(data['relevé']['saes'], ues)
 
-        let obj = {};
-        obj[semesterID] = ues;
-        chrome.storage.sync.set(obj);
+        const obj = {}
+        obj[semesterID] = ues
+        chrome.storage.sync.set(obj)
 
-        return ues;
-    });
+        return ues
+    })
 }
 
 function addRessources(ressources, ues) {
     for (const ressource of Object.entries(ressources)) {
-        const ressourceName = ressource[0];
+        const [ressourceName, ressourceData] = ressource
 
-        for (const evaluation of ressource[1].evaluations) {
+        for (const evaluation of ressourceData.evaluations) {
             for (const weight of Object.entries(evaluation.poids)) {
                 const ueName = weight[0]
 
-                if (weight[1] != 0) {
+                if (weight[1] !== 0) {
                     if (!(ueName in ues)) {
-                        ues[ueName] = {name: ueName, ressources: {}};
+                        ues[ueName] = {
+                            name: ueName,
+                            ressources: {}
+                        }
                     }
                     if (!(ressourceName in ues[ueName].ressources)) {
-                        ues[ueName].ressources[ressourceName] = {name: ressourceName, titre: ressource[1].titre, weight: 0};
+                        ues[ueName].ressources[ressourceName] = {
+                            name: ressourceName,
+                            titre: ressourceData.titre,
+                            weight: 0
+                        }
                     }
                 }
             }
