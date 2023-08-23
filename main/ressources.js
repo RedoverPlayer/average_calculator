7cf67fbac6c45561d82f401f5d62d5c7eb83df3c
Kensaa <kensadevs@gmail.com>
2023-08-23 19:21:03 +0200

    cleaning in ressources.js

--- main/ressources.js
+++ main/ressources.js
@@ -1,24 +1,24 @@
 // ---- Ressources & SAEs ----
 function displayRessources(ressources, isSAE = false) {
-    let ressourcesDiv = document.getElementById("ressources");
-    let saesDiv = document.getElementById("saes");
+    const ressourcesDiv = document.getElementById("ressources");
+    const saesDiv = document.getElementById("saes");
 
     if (!isSAE) { ressourcesDiv.innerHTML = "" } else { saesDiv.innerHTML = "" }
 
-    for (let ressource of Object.entries(ressources)) {
-        let ressourceUl = document.createElement("ul");
+    for (const ressource of Object.entries(ressources)) {
+        const ressourceUl = document.createElement("ul");
         ressourceUl.className = "list-group";
 
         // Ressource title
-        let ressourceLi = document.createElement("button");
+        const ressourceLi = document.createElement("button");
         ressourceLi.className = "list-group-item bg-success d-flex justify-content-between text-light";
         ressourceLi.onclick = (event) => { toggleEvals(event) };
 
-        let ressourceTitle = document.createElement("div");
+        const ressourceTitle = document.createElement("div");
         ressourceTitle.innerText = `${ressource[0]} - ${ressource[1].titre}`
         ressourceLi.appendChild(ressourceTitle);
 
-        let ressourceAverage = document.createElement("div");
+        const ressourceAverage = document.createElement("div");
 
         // add to DOM
         ressourceLi.appendChild(ressourceAverage);
@@ -35,25 +35,27 @@ function displayEvals(evals, ressourceAverage, ressourceUl, ressourceLi, isSAE) 
     let total = 0;
     let coefTotal = 0;
 
-    for (let eval of evals) {
+    for (const eval of evals) {
         // Add eval to ressource average
-        if (!isNaN(parseFloat(eval.note.value)) && !isNaN(parseFloat(eval.coef))) {
-            total += parseFloat(eval.note.value) * parseFloat(eval.coef);
-            coefTotal += parseFloat(eval.coef);
+        const note = parseFloat(eval.note.value);
+        const coef = parseFloat(eval.coef);
+        if (!isNaN(note) && !isNaN(coef)) {
+            total += note * coef;
+            coefTotal += coef;
         }
 
         // Create eval elem
-        let evalLi = document.createElement("li");
+        const evalLi = document.createElement("li");
         evalLi.className = "list-group-item d-flex gap-5 pt-1 pb-1 pl-2 pr-2";
         evalLi.title = `Min. ${eval.note.min}, Max ${eval.note.max}`;
 
         // Eval title
-        let evalTitle = document.createElement("div");
+        const evalTitle = document.createElement("div");
         evalTitle.innerText = eval.description;
         evalLi.appendChild(evalTitle);
 
         // Eval note
-        let evalNote = document.createElement("div");
+        const evalNote = document.createElement("div");
         evalNote.innerText = eval.note.value;
         // Display badges indicating if the note is the max or min of the promo
         if (eval.note.value == eval.note.max) {
@@ -67,14 +69,14 @@ function displayEvals(evals, ressourceAverage, ressourceUl, ressourceLi, isSAE) 
         evalLi.appendChild(evalNote);
 
         // Eval moy
-        let evalMoy = document.createElement("div");
+        const evalMoy = document.createElement("div");
         evalMoy.innerText = `Moy. ${eval.note.moy}`;
         evalMoy.className = "text-secondary d-flex align-items-center";
         evalMoy.style.minWidth = "4.9rem";
         evalLi.appendChild(evalMoy);
 
         // Eval coef
-        let evalCoef = document.createElement("div");
+        const evalCoef = document.createElement("div");
         evalCoef.innerText = `Coef. ${eval.coef}`;
         evalCoef.className = "text-secondary d-flex align-items-center";
         evalCoef.style.minWidth = "5rem";
@@ -82,13 +84,13 @@ function displayEvals(evals, ressourceAverage, ressourceUl, ressourceLi, isSAE) 
 
         // Default collapse
         if (!isSAE) {
-            chrome.storage.sync.get('ressourcesDevelopped').then(function (result) {
+            chrome.storage.sync.get('ressourcesDevelopped').then(result => {
                 if (!result.ressourcesDevelopped) {
                     evalLi.style.setProperty('display', 'none', 'important');
                 }
             }); 
         } else {
-            chrome.storage.sync.get('saesDevelopped').then(function (result) {
+            chrome.storage.sync.get('saesDevelopped').then(result => {
                 if (!result.saesDevelopped) {
                     evalLi.style.setProperty('display', 'none', 'important');
                 }
@@ -98,7 +100,7 @@ function displayEvals(evals, ressourceAverage, ressourceUl, ressourceLi, isSAE) 
         ressourceUl.appendChild(evalLi);
     }
 
-    let average = (total / coefTotal).toFixed(2);
+    const average = (total / coefTotal).toFixed(2);
     ressourceAverage.innerText = `${average}`;
 
     // Set ressource color depending on average. Orange between 8 and 10, red under 8
@@ -111,10 +113,9 @@ function displayEvals(evals, ressourceAverage, ressourceUl, ressourceLi, isSAE) 
 }
 
 function toggleEvals(event) {
-    let target = event.target;
-    if (event.target.nodeName == "DIV") target = event.target.parentNode;
+    const target = event.target.nodeName == "DIV" ? event.target.parentNode : event.target;
 
-    for (let child of target.parentNode.childNodes) {
+    for (const child of target.parentNode.childNodes) {
         if (child.nodeName == "LI" && child.style.display != "none") {
             child.style.setProperty('display', 'none', 'important');
         } else {
